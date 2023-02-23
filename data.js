fbaf914d040b049301893f8a97334c8e17917713
Sitanshu Lall <sitanshulall0@gmail.com>
2023-02-24 00:14:43 +0530

    commit

---
+++ data.js
@@ -0,0 +1,38 @@
+let songs = [
+{
+    name: 'Bin Tere',
+    path: 'music/Bin Tere.mp3',
+    artist: 'Shafqat Amanat Ali, Sunidhi Chauhan, Vishal–Shekhar',
+    cover: 'images/BinTere.jpg',
+},
+{
+    name: 'Ghungroo',
+    path: 'music/Ghungroo .mp3',
+    artist: 'Arijit Singh, Shilpa Rao, Vishal-Shekhar',
+    cover: 'images/Ghungroo.jpg',
+},
+{
+    name: 'Ilahi',
+    path: 'music/Ilahi.mp3',
+    artist: 'Arijit Singh',
+    cover: 'images/Ilahi.jpg',
+},
+{
+    name: 'Tere Naina',
+    path: 'music/Tere Naina.mp3',
+    artist: 'Shankar-Ehsaan-Loy, Shafqat Amanat Ali',
+    cover: 'images/Tere Naina.jpg',
+},
+{
+    name: 'Ek Main Aur Ek Tu',
+    path: 'music/Ek Main Aur Ekk Tu.mp3',
+    artist: 'Benny Dayal, Anushka Manchanda, Shefali Alvares',
+    cover: 'images/Ek Main Aur Ek Tu.jpg',
+},
+{
+    name: 'Ayee Na Humara Bihar Main',
+    path: 'music/Ayee Na Humara Bihar Main.mp3',
+    artist: 'Advait Nemlekar',
+    cover: 'images/Ayee Na Humara Bihar Main.jpg',
+}
+]
